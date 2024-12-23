3aaa06396a0061a27a989471f09a1ba171503a19
Heesom <heesom.hs@gmail.com>
2024-12-23 17:24:36 +0900

    file: done making server

--- src/main/java/com/bookie/scrap/startup/Main.java
+++ src/main/java/com/bookie/scrap/startup/Main.java
@@ -4,27 +4,46 @@ import com.bookie.scrap.config.watcha.WatchaBookConfig;
 import com.bookie.scrap.http.HttpRequestExecutor;
 import com.bookie.scrap.response.watcha.WatchaBookDetail;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 
-//@Slf4j
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        String logbackPath = System.getProperty("logback.configurationFile");
-        String dbPropertiesPath = System.getProperty("db.properties");
 
+        System.out.println("Is logback.xml loaded? " +
+                LoggerFactory.getILoggerFactory().getClass().getName());
+
+
+        log.info("");
+        log.info("*********************************************");
+        log.info("   Initializing......                        ");
+        log.info("       Bookie Scrap Version: 0.0.1-SNAPSHOT  ");
+        log.info("                                             ");
+        log.info("*********************************************");
+        log.info("");
+
+        // TODO: 잘라서 resources 로딩 로그, 라이브러리 로딩 로그 따로 출력 해야함
         String classPath = System.getProperty("java.class.path");
-        System.out.println("Classpath: " + classPath);
+        log.info("Classpath: " + classPath);
+        log.info("");
 
-//        System.out.println("Logback Path: " + logbackPath);
-//        System.out.println("DB Properties Path: " + dbPropertiesPath);
-//        WatchaBookDetail book = HttpRequestExecutor.execute(new WatchaBookConfig("byLKj8M"));
+        // TODO: 후에 운영/개발 분리
+        try (InputStream inpuStream = Main.class.getClassLoader().getResourceAsStream("db.properties")){
+            Properties dbProperties = new Properties();
+            dbProperties.load(inpuStream);
+            log.info("DB URL: {}",dbProperties.getProperty("db.dev.url"));
+            log.info("DB USER: {}",dbProperties.getProperty("db.dev.user"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-//        log.info("");
-//        log.info("*********************************************");
-//        log.info("   Initializing......                        ");
-//        log.info("       Bookie Scrap Version: 0.0.1-SNAPSHOT  ");
-//        log.info("                                             ");
-//        log.info("*********************************************");
-//        log.info("");
+
+        log.info("*********************************************");
+
     }
 }
