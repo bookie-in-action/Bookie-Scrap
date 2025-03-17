83bee5153d46bcb22c8c0d79d8d687c93b29a031
Heesom <heesom.hs@gmail.com>
2025-03-17 13:42:59 +0900

    기능: watcha 메인 로직 (진행중)

--- src/main/java/com/bookie/scrap/watcha/scheduler/WatchaJob.java
+++ src/main/java/com/bookie/scrap/watcha/scheduler/WatchaJob.java
@@ -1,28 +1,47 @@
 package com.bookie.scrap.watcha.scheduler;
 
+import com.bookie.scrap.common.request.Request;
+import com.bookie.scrap.common.request.RequestFactory;
+import com.bookie.scrap.watcha.domain.WatchaBaseRequestParam;
+import com.bookie.scrap.watcha.dto.WatchaBookcaseDTO;
 import com.bookie.scrap.watcha.entity.WatchaBookEntity;
 import com.bookie.scrap.common.Repository;
-import com.bookie.scrap.watcha.repository.WatchaBookRepository;
-import com.bookie.scrap.watcha.request.WatchaBookRequestFactory;
-import com.bookie.scrap.watcha.dto.WatchaBookDto;
+import com.bookie.scrap.watcha.repository.WatchaBookMetaRepository;
+import com.bookie.scrap.watcha.request.WatchaBookMetaRequestFactory;
+import com.bookie.scrap.watcha.dto.WatchaBookMetaDto;
+import com.bookie.scrap.watcha.request.WatchaBookcaseRequestFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.List;
+
 
 @Slf4j
 public class WatchaJob implements Job {
 
-    private final Repository<WatchaBookEntity> bookRepository = WatchaBookRepository.getInstance();
-    private final WatchaBookRequestFactory bookRequestFactory = WatchaBookRequestFactory.getInstance();
+    private final RequestFactory<List<WatchaBookcaseDTO>> bookcaseRequestFactory = WatchaBookcaseRequestFactory.getInstance();
+    private final RequestFactory<WatchaBookMetaDto> bookRequestFactory = WatchaBookMetaRequestFactory.getInstance();
+
+    private final Repository<WatchaBookEntity> bookMetaRepository = WatchaBookMetaRepository.getInstance();
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
-        log.info("watcha job executed!!!!!");
+        WatchaBaseRequestParam requestParam = new WatchaBaseRequestParam("gcdkyKnXjN", 1, 12);
+        Request<List<WatchaBookcaseDTO>> bookcaseRequest = bookcaseRequestFactory.createRequest(requestParam);
+        List<WatchaBookcaseDTO> bookcaseList = bookcaseRequest.execute();
 
-        WatchaBookDto bookDetail = bookRequestFactory.createRequest("byLKj8M").execute();
-        boolean result = bookRepository.insertOrUpdate(bookDetail.toEntity());
+        for (WatchaBookcaseDTO bookcaseDTO : bookcaseList) {
+            // bookcase repo에 저장
+
+            String bookCode = bookcaseDTO.getBookCode();
+            Request<WatchaBookMetaDto> bookMetaRequest = bookRequestFactory.createRequest(bookCode);
+            WatchaBookMetaDto bookMetaDto = bookMetaRequest.execute();
+            bookMetaRepository.insertOrUpdate(bookMetaDto.toEntity());
+
+            // comment repo에 저장
+        }
 
     }
 }
