151cce22b254bec075a0e6188805bcad8671088b
Heesom <heesom.hs@gmail.com>
2025-03-24 00:07:56 +0900

    파일: WatchaEntity->BaseEntity로 이름 변경 후 패키지 이동

--- src/main/java/com/bookie/scrap/watcha/entity/WatchaBookEntity.java
+++ src/main/java/com/bookie/scrap/watcha/entity/WatchaBookEntity.java
@@ -1,5 +1,6 @@
 package com.bookie.scrap.watcha.entity;
 
+import com.bookie.scrap.common.domain.BaseEntity;
 import com.bookie.scrap.common.util.ListStringConverter;
 import jakarta.persistence.*;
 import lombok.*;
@@ -12,7 +13,7 @@ import java.util.List;
 @Entity @SuperBuilder
 @Table(name = "BS_WATCHA_BOOK")
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
-public final class WatchaBookEntity extends WatchaEntity {
+public final class WatchaBookEntity extends BaseEntity {
 
     @Column(name = "book_code")
     private String bookCode;
