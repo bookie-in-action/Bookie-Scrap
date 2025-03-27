1ea75c2068add43cbe919fefdaf2651d077c8e50
Heesom <heesom.hs@gmail.com>
2025-03-27 17:13:34 +0900

    기능: redis와 연결하고 저장하는 기능 추가

---
+++ src/main/java/com/bookie/scrap/common/db/redis/RedisConnectionProducer.java
@@ -0,0 +1,26 @@
+package com.bookie.scrap.common.db.redis;
+
+import com.bookie.scrap.common.lifecycle.Shutdownable;
+import com.bookie.scrap.common.properties.DbProperties;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+
+public class RedisConnectionProducer implements Shutdownable {
+
+    private static final DbProperties dbProperties = DbProperties.getInstance();
+
+    private static final RedisClient redisClient;
+
+    static {
+        redisClient = RedisClient.create(dbProperties.getValue(DbProperties.Key.REDIS_URL));
+    }
+    
+    public static StatefulRedisConnection<String, String> getConn() {
+        return redisClient.connect();
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+        redisClient.shutdown();
+    }
+}
