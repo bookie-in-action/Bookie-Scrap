ef9c66d15da11837eef45e63b15a960903cf847e
Heesom <heesom.hs@gmail.com>
2025-06-30 11:36:13 +0900

    기능: ScraperJob에 execute 메서드 추가해 테스트 용이하게 만듬

--- src/test/java/com/bookie/scrap/ScrapAppTest.java
+++ src/test/java/com/bookie/scrap/ScrapAppTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
